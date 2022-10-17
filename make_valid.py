ebb9a615112fb767dd1d343f389dcaba86d898b5
PangContest <115514023+PangContest@users.noreply.github.com>
2022-10-17 14:43:51 +0900

    Update make_valid.py

--- make_valid.py
+++ make_valid.py
@@ -1,8 +1,9 @@
 import os
 import shutil
 from tqdm.auto import tqdm
+import cv2
 
-train_data_path = 'D:/Deep/super'
+train_data_path = 'D:/Data/super'
 edit_data_path = './data'
 
 train_hr_file_list = os.listdir(train_data_path+'/train/hr')
@@ -32,7 +33,14 @@ for filename in tqdm(train_hr_file_list):
         shutil.copy(train_data_path + '/train/hr/' +filename, edit_data_path + '/train/hr/' +filename)
         shutil.copy(train_data_path + '/train/lr/' + filename, edit_data_path + '/train/lr/' + filename)
 
+test_lr_file_list = os.listdir(train_data_path+'/test/lr')
 
+createFolder(edit_data_path+'/test')
+createFolder(edit_data_path+'/test/hr')
+createFolder(edit_data_path+'/test/lr')
 
-
-
+for filename in tqdm(test_lr_file_list):
+    shutil.copy(train_data_path + '/test/lr/' + filename, edit_data_path + '/test/lr/' + filename)
+    image = cv2.imread(train_data_path + '/test/lr/' + filename)
+    resized_image = cv2.resize(image, (2048, 2048))
+    cv2.imwrite(edit_data_path + '/test/hr/' + filename,resized_image)
